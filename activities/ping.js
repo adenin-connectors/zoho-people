be0817aef31c6d284e7b63c054d71d132f1a33b5
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-04-24 09:56:25 +0200

    Removed global variable Activity.

--- activities/ping.js
+++ activities/ping.js
@@ -3,19 +3,20 @@ const api = require('./common/api');
 
 module.exports = async (activity) => {
   try {
+    api.initialize(activity);
     //sending wrong token after initial authorization returns success
     //might be because I am getting error:
     //response.body.response.errors.code => 7074
     //"API is unavailable for your pricing plan. Please upgrade to access"
     const response = await api('/announcement/getAllAnnouncement?startIdx=1');
 
-    if (Activity.isErrorResponse(response)) return;
+    if ($.isErrorResponse(activity, response)) return;
 
     activity.Response.Data = {
       success: response && response.statusCode === 200
     };
   } catch (error) {
-    Activity.handleError(error);
+    $.handleError(activity, error);
     activity.Response.Data.success = false;
   }
 };
